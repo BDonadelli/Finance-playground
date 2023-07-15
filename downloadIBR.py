7c5ee2e76cee1fd6d9197aea3b7ce866e003e59c
Benjamin <jdonadelli@gmail.com>
2023-07-15 17:25:20 -0300

    ajustes

--- downloadIBR.py
+++ downloadIBR.py
@@ -1,21 +1,22 @@
-#
-# Download de carteiras da B3
-#
-
 from selenium import webdriver
 from time import sleep
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver import ChromeOptions
+from selenium.webdriver import ChromeOptions, Chrome, Keys
 
 #Chrome
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
+#opts = ChromeOptions()
+#esta opcao serve para nao fechar o navegador apos a execucao do script
+#options.add_experimental_option("detach", True)
+
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_experimental_option("prefs", {
-  "download.default_directory": r"/home/yair/Dropbox/Download/",
+  "download.default_directory": r"/home/yair/",
   "download.prompt_for_download": False,
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True
@@ -24,27 +25,26 @@ options.add_experimental_option("prefs", {
 servico=Service(ChromeDriverManager().install())
 driver=webdriver.Chrome(service=servico, options=options)
 
+
 url=[
     'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-100-ibrx-100-composicao-da-carteira.htm', #IBR100
     'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-50-ibrx-50-composicao-da-carteira.htm',#IBR50
     'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-ibovespa-ibovespa-composicao-da-carteira.htm' #IBOV
     ]
 
-for sitio in url:
-
+for sitio in  url :
     driver.get(sitio)
-
     sleep(2)
-    driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
-
-
-    driver.implicitly_wait(3) # seconds
-
+    if sitio == url[0] :
+        driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
+        driver.implicitly_wait(3) # seconds
     driver.switch_to.frame("bvmf_iframe")
-
     driver.find_element(By.CLASS_NAME , 'primary-text').find_element(By.TAG_NAME,"a").click()
+    sleep(3)
 
-    driver.implicitly_wait(2) # seconds
+driver.close()    
+
+
 
 
 ##XPATH,'//*[@id="divContainerIframeB3"]/div/div[1]/form/div[2]/div/div[2]/div/div/div[1]/div[2]/p/a').click()
