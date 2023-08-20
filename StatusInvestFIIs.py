147ff915210defb0e662d7ba1bb8ab2d8461fca6
Benjamin <jdonadelli@gmail.com>
2023-08-20 18:03:14 -0300

    L&S

--- StatusInvestFIIs.py
+++ StatusInvestFIIs.py
@@ -1,13 +1,10 @@
 from selenium import webdriver
 from time import sleep
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver import ChromeOptions, Chrome, Keys
-
 #Chrome
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
-
 opts = ChromeOptions()
 #esta opcao serve para nao fechar o navegador apos a execucao do script
 opts.add_experimental_option("detach", True)
@@ -17,50 +14,31 @@ opts.add_experimental_option("prefs", {
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True
 })
-
 servico=Service(ChromeDriverManager().install())
 driver=webdriver.Chrome(service=servico, options=opts)
-
+###########################################################################
+# endereço da planilha 
 url2='https://statusinvest.com.br/fundos-imobiliarios/busca-avancada'
-driver.get(url2)
-sleep(4)
-
+# caminho html para o botão "buscar"
 path='//div/button[contains(@class,"find")]'
+# caminho html para o botão "download"
 path2='//div/a[contains(@class,"btn-download")]'
 
-driver.find_element(By.XPATH,path).click()
-sleep(20)#espera anuncio
-# anuncio = 
-driver.find_elements(By.CLASS_NAME,'btn-close')[0].click()
+## ações
+# abre navegador
+driver.get(url2)
+# espera carregar a pagina
 sleep(3)
-# download
+# clica em buscar
+driver.find_element(By.XPATH,path).click()
+sleep(3)
+# espera um anuncio,
+sleep(16) 
+# fecha o anuncio clicando no "x"
+driver.find_elements(By.CLASS_NAME,'btn-close')[0].click()
+# clica download
 driver.find_element(By.XPATH,path2).click()
-
+# espera o download
 sleep(15)
+# fecha o navegador
 driver.close()
-
-import pandas as pd
-
-df = pd.read_csv('statusinvest-busca-avancada (1).csv', 
-                 sep=';' , decimal=',' , header = 0, index_col=False ,  thousands='.' , 
-                 encoding='latin1')
-df = df.fillna('')
-
-print(df.head())
-
-# # agora atualiza planilha diretamente no googlesheets
-# import gspread
-# from oauth2client.service_account import ServiceAccountCredentials
-# scope = ['https://www.googleapis.com/auth/drive','https://www.googleapis.com/auth/spreadsheets']
-# jfile = 'carteira-328314-d38dcc8ee3e4.json'
-# credentials = ServiceAccountCredentials.from_json_keyfile_name(jfile, scope)
-# gc = gspread.authorize(credentials)
-
-# planilha = gc.open('Dados')
-# pagina = planilha.worksheet("StatusInvest-FIIs")
-
-
-# pagina.update('b2', [df.columns.values.tolist()] + df.values.tolist())
-# # # registra data da ultima atualização
-# from datetime import date
-# pagina.update('a1',date.today().strftime('%d/%m/%Y'))
