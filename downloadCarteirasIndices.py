415d71da8bb36e1a07c5dc7aa66c71e552f8b9f1
Benjamin <jdonadelli@gmail.com>
2023-12-15 17:09:32 -0300

    csv carteiras

--- downloadCarteirasIndices.py
+++ downloadCarteirasIndices.py
@@ -16,7 +16,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_experimental_option("prefs", {
-  "download.default_directory": r"/home/yair/GHub/Codigos-em-financas/",
+  "download.default_directory": r"/home/yair/GHub/Codigos-em-financas/data/",
   "download.prompt_for_download": False,
   "download.directory_upgrade": True,
   "safebrowsing.enabled": True
@@ -27,15 +27,16 @@ driver=webdriver.Chrome(service=servico, options=options)
 
 
 url=[
-    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-100-ibrx-100-composicao-da-carteira.htm', #IBR100
-    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-50-ibrx-50-composicao-da-carteira.htm',#IBR50
-    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-ibovespa-ibovespa-composicao-da-carteira.htm', #IBOV
-    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/indice-dividendos-idiv-composicao-da-carteira.htm' #IDIV
+    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-100-ibrx-100-composicao-da-carteira.htm',   #IBR100
+    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-50-ibrx-50-composicao-da-carteira.htm',     #IBR50
+    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-ibovespa-ibovespa-composicao-da-carteira.htm',     #IBOV
+    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/indice-dividendos-idiv-composicao-da-carteira.htm', #IDIV
+    'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-de-segmentos-e-setoriais/indice-small-cap-smll-composicao-da-carteira.htm'
     ]
 
 for sitio in  url :
     driver.get(sitio)
-    sleep(2)
+    sleep(3)
     if sitio == url[0] :
         driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
         driver.implicitly_wait(3) # seconds
@@ -46,15 +47,17 @@ for sitio in  url :
 driver.close()    
 
 
+# driver.get(url[0])
+# sleep(3)
+# driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
+# driver.implicitly_wait(3) # seconds
+# driver.switch_to.frame("bvmf_iframe")
+# driver.find_element(By.CLASS_NAME , 'primary-text').find_element(By.TAG_NAME,"a").click()
 
 
-##XPATH,'//*[@id="divContainerIframeB3"]/div/div[1]/form/div[2]/div/div[2]/div/div/div[1]/div[2]/p/a').click()
-
-#url='https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-ibovespa-ibovespa-composicao-da-carteira.htm'
-#sleep(2)
-#driver.get(url)
-#sleep(2)
-## driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
-## driver.implicitly_wait(3) # seconds
-#driver.switch_to.frame("bvmf_iframe")
-#driver.find_element(By.XPATH,'//*[@id="divContainerIframeB3"]/div/div[1]/form/div[2]/div/div[2]/div/div/div[1]/div[2]/p/a').click()
+# river.get(url[1])
+# sleep(3)
+# driver.find_element(By.ID,'onetrust-accept-btn-handler').click()
+# driver.implicitly_wait(3) # seconds
+# driver.switch_to.frame("bvmf_iframe")
+# driver.find_element(By.CLASS_NAME , 'primary-text').find_element(By.TAG_NAME,"a").click()
