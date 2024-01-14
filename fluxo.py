b5f7faf4f2f7e8ac2ad530a301c31fb99b50e526
Benjamin <jdonadelli@gmail.com>
2024-01-14 19:30:30 -0300

    teste de grafico em html, csv

--- fluxo.py
+++ fluxo.py
@@ -1,36 +1,42 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ChromeOptions, Chrome, Keys
+if str(input('download? (s/n)')) == 's' :
+  from selenium import webdriver
+  from selenium.webdriver.common.by import By
+  from selenium.webdriver import ChromeOptions, Chrome, Keys
 
-#Chrome
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+  #Chrome
+  from selenium.webdriver.chrome.service import Service
+  from webdriver_manager.chrome import ChromeDriverManager
 
-opts = webdriver.ChromeOptions()
-#esta opcao serve para nao fechar o navegador apos a execucao do script
-opts.add_experimental_option("detach", True)
-opts.add_experimental_option("prefs", {
-  "download.default_directory": r"/home/yair/GHub/Codigos-em-financas/data",
-  "download.prompt_for_download": False,
-  "download.directory_upgrade": True,
-  "safebrowsing.enabled": True
-})
+  opts = webdriver.ChromeOptions()
+  #esta opcao serve para nao fechar o navegador apos a execucao do script
+  opts.add_experimental_option("detach", True)
+  opts.add_experimental_option("prefs", {
+    "download.default_directory": r"/home/yair/GHub/Codigos-em-financas/data",
+    "download.prompt_for_download": False,
+    "download.directory_upgrade": True,
+    "safebrowsing.enabled": True
+  })
 
-servico=Service(ChromeDriverManager().install())
-driver=webdriver.Chrome(service=servico, options=opts)
+  servico=Service(ChromeDriverManager().install())
+  driver=webdriver.Chrome(service=servico, options=opts)
 
-from time import sleep
+  from time import sleep
 
-url='https://www.dadosdemercado.com.br/bolsa/investidores-estrangeiros'
-driver.get(url)
-sleep(2)
+  url='https://www.dadosdemercado.com.br/bolsa/investidores-estrangeiros'
+  driver.get(url)
+  sleep(2)
 
-path='//*[@id="download-csv"]'
-botao = driver.find_element(By.XPATH,path)
-botao.click()
+  path='//*[@id="download-csv"]'
+  botao = driver.find_element(By.XPATH,path)
+  botao.click()
 
-sleep(5)
-driver.quit()
+  sleep(5)
+  driver.quit()
+
+''' 
+daqui pra baixo usa .csv do 
+ultimo download
+'''
 
 import pandas as pd
 import sys,csv
@@ -56,6 +62,18 @@ from datetime import datetime
 df['Data'] = pd.to_datetime(df['Data'] , dayfirst=True)
 df.set_index('Data',inplace=True)
 
+print( 'inicio',df.index[0].date() )
+print( 'fim', df.index[-1].date())
+
+
+termino = df.index[0].date()
+inicio  = df.index[-1].date()
+
+import yfinance as yf
+indice = yf.download('^BVSP' , start=inicio , end=termino , auto_adjust=True)['Close']
+
+print(indice.head())
+
 print(df.head())
 print(df.info())
 
@@ -63,8 +81,9 @@ import plotly.graph_objects as go
 
 fig = go.Figure(data=[
   go.Bar(x=df.index, y=df.Estrangeiro,name='Estrangeiro'),
-  go.Bar(x=df.index, y=df.Institucional,name='Institucional'),
-  go.Bar(x=df.index, y=df['Pessoa física'],name='PF')
+  go.Bar(x=df.index, y=df.Institucional,name='Institucional',visible="legendonly"),
+  go.Bar(x=df.index, y=df['Pessoa física'],name='PF',visible="legendonly"),
+  go.Scatter(x=indice.index,y=(indice.values-110000)/10,mode='lines',name='Ibov',line=dict(color='red'))
 ])
 fig.show()
 
