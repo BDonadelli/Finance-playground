2a88fd253adb1fb643987c31baaa645a6f9c8d73
Benjamin <jdonadelli@gmail.com>
2024-08-23 12:25:09 -0300

    script atualiza dados do tesouro

---
+++ DT_TesouroDireto_Dia.py
@@ -0,0 +1,33 @@
+
+'''
+versão de DT_TesouroDireto_Dia_planilha_v2.ipynb
+para executar no terminal 
+atualiza 'data/tesouro_direto.json'
+reminder: fazer push
+'''
+
+url = 'https://www.tesourodireto.com.br/json/br/com/b3/tesourodireto/service/api/treasurybondsinfo.json'
+
+from selenium import webdriver
+driver=webdriver.Chrome()
+driver.get(url)
+
+page_source = driver.page_source
+start = page_source.find("{")
+end = page_source.rfind("}") + 1
+json_text = page_source[start:end]
+
+import json
+data_json = json.loads(json_text)
+
+driver.close()
+
+with open('data/tesouro_direto.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data_json, json_file, ensure_ascii=False, indent=4)
+
+import git
+repo = git.Repo('.')
+repo.git.add(A=True)
+repo.index.commit("Atualiza 'data/tesouro_direto.json'")
+origin = repo.remote(name='origin')
+origin.push()
