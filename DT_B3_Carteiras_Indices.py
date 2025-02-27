39a7ecd291a93cbedc548b2a13df4f10f139a920
Benjamin <jdonadelli@gmail.com>
2025-02-27 09:45:07 -0300

    Atualização automatica (todo repo) para tesouro_direto.json

--- DT_B3_Carteiras_Indices.py
+++ DT_B3_Carteiras_Indices.py
@@ -16,7 +16,7 @@ import os
 from DT_atualiza_settings import *
 
 url=[
-    # 'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-amplo-ibra-composicao-da-carteira.htm',
+     'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-amplo-ibra-composicao-da-carteira.htm',
     # 'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-100-ibrx-100-composicao-da-carteira.htm',   #IBR100
     # 'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-brasil-50-ibrx-50-composicao-da-carteira.htm',     #IBR50
     'https://www.b3.com.br/pt_br/market-data-e-indices/indices/indices-amplos/indice-ibovespa-ibovespa-composicao-da-carteira.htm',     #IBOV
